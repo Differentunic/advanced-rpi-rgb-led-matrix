df79ed245238300abf4d60cbac52ae43bbb8ab7b
Differentunic <66045103+Differentunic@users.noreply.github.com>
2023-08-29 11:29:49 +1000

    Update Setup.sh

--- utils/Setup.sh
+++ utils/Setup.sh
@@ -66,7 +66,7 @@ clone_repo_and_run_make() {
 main() {
 
     log "INFO" "Starting package installation..."
-    packages=("libgraphicsmagick++-dev" "libwebp-dev" "git" "build-essentials")
+    packages=("libgraphicsmagick++-dev" "libwebp-dev" "git" "build-essential")
     install_packages "${packages[@]}"
     log "INFO" "Package installation completed."
 
